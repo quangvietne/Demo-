84a1262e0f12d327d8623b0cebffea3c85cd1e73
quangvietne <152321981+quangvietne@users.noreply.github.com>
2024-09-02 14:37:22 +0700

    Add files via upload

---
+++ Buoi_1.2.3.py
@@ -0,0 +1,12 @@
+# câu lệnh print(object , sep , end)
+# sep :Phân cách giữa các đối tượng khi in (mặc định là dấu cách) 
+# end :Chỉ ra kí tự đc in ở cuối dòng (mặc định là xuống dòng) 
+# print : có thể dùng dấu nháy đơn ( ' ') or nháy kép (" ")
+""" Đây là chú thích trên nhiều dòng ( 3 dấu nháy kép or 3 dấu nháy đơn ) """
+a = 100 ; 
+b = 200 ; 
+c = 300 ; 
+print( a  , b , c )
+print( a , b , c , sep = '--' )
+print ('Hello world ')
+print ("Hello world")
